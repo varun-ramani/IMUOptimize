add5094fbc58582fed77df63afc795be4fd69971
Varun Ramani <varun.ramani@gmail.com>
2024-01-14 18:53:57 -0500

    added num sensors parameter to transformed amass

--- src/data/load/transformed_amass.py
+++ src/data/load/transformed_amass.py
@@ -8,11 +8,12 @@ from torch.utils.data import Dataset, DataLoader
 import random
 
 class AMASSDataset(Dataset):
-    def __init__(self, amass_dir: str, shuffle=True):
+    def __init__(self, amass_dir: str, shuffle=True, num_sensors=24):
         self.amass_dir = Path(amass_dir)
         self.sequences = list(self.amass_dir.glob('**/seq*/'))
         if shuffle:
             random.shuffle(self.sequences)
+        self.num_sensors = num_sensors
 
     def __len__(self):
         return len(self.sequences)
@@ -23,8 +24,8 @@ class AMASSDataset(Dataset):
         )
 
         poses = poses.view(-1, 24 * 3 * 3)
-        vacc = vacc.view(-1, 24 * 3)
-        vrot = vrot.view(-1, 24 * 3 * 3)
+        vacc = vacc.view(-1, self.num_sensors * 3)
+        vrot = vrot.view(-1, self.num_sensors * 3 * 3)
 
         inputs = torch.hstack((vacc, vrot))
         outputs = poses
