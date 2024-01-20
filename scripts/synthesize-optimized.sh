6ff3844aa7a34e121962468c5b5515b563067aae
Varun Ramani <varun.ramani@gmail.com>
2024-01-19 20:51:54 -0500

    fixed bug in ordering of joints in synthesis optimized

--- scripts/synthesize-optimized.sh
+++ scripts/synthesize-optimized.sh
@@ -6,6 +6,6 @@ python -m data.synthesize \
           --input $ARTIFACTS/data/AMASS \
           --output $ARTIFACTS/data/AMASS_t_optim \
           --model $ARTIFACTS/smpl/SMPL_male.pkl \
-          --joints "9 21 2 12 15 0" \
+          --joints "0 9 21 2 12 15" \
           --purge-existing \
           --keep-subdirectories
